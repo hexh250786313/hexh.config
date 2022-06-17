4a31dddc90a38a64c297f4843d292d30a22bf0a2
hexh <250786313@qq.com>
2022-06-17 23:15:54 +0800

    test

--- src/env/manjaro/modules/proxy/generateClashDir.ts
+++ src/env/manjaro/modules/proxy/generateClashDir.ts
@@ -3,25 +3,47 @@ import { existsSync } from "fs-extra";
 import { homedir } from "os";
 
 export default async function generateClashDir(timeout?: number) {
-  try {
-    await runCommand("pkill -e clash");
-  } catch (e) {
-    /* handle error */
-  }
+  return new Promise((resolve) => {
+    runCommand("pkill -e clash")
+      .then(async () => {
+        setTimeout(async () => {
+          if (!existsSync(`${homedir()}/.config/clash/cache.db`)) {
+            process.stdout.write(
+              "Failed to generate clash directory, trying again...\n"
+            );
+            await generateClashDir();
+          } else {
+            await runCommand("pkill -e clash");
+            process.stdout.write(
+              "OJBK generating clash directory, please your clash and click https://clash.razord.top to select a global proxy.\n"
+            );
+            resolve(null);
+          }
+        }, timeout || 60000);
+        await runCommand("clash &");
+      })
+      .catch(() => {
+        /* handle error */
+      });
+  });
 
-  setTimeout(async () => {
-    if (!existsSync(`${homedir()}/.config/clash/cache.db`)) {
-      process.stdout.write(
-        "Failed to generate clash directory, trying again...\n"
-      );
-      await generateClashDir();
-    } else {
-      await runCommand("pkill -e clash");
-      process.stdout.write(
-        "OJBK generating clash directory, please your clash and click https://clash.razord.top to select a global proxy.\n"
-      );
-    }
-  }, timeout || 60000);
-
-  await runCommand("clash &");
+  // try {
+  // await runCommand("pkill -e clash");
+  // } catch (e) {
+  // /* handle error */
+  // }
+  // setTimeout(async () => {
+  // if (!existsSync(`${homedir()}/.config/clash/cache.db`)) {
+  // process.stdout.write(
+  // "Failed to generate clash directory, trying again...\n"
+  // );
+  // await generateClashDir();
+  // } else {
+  // await runCommand("pkill -e clash");
+  // process.stdout.write(
+  // "OJBK generating clash directory, please your clash and click https://clash.razord.top to select a global proxy.\n"
+  // );
+  // }
+  // }, timeout || 60000);
+  // await runCommand("clash &");
 }
