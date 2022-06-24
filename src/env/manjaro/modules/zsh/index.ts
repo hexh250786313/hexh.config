907ee847a0ead0c028dafb4cd7bfde714e0cc459
hexh <250786313@qq.com>
2022-06-24 15:22:31 +0800

    wip

--- src/env/manjaro/modules/zsh/index.ts
+++ src/env/manjaro/modules/zsh/index.ts
@@ -32,7 +32,7 @@ export default class Zsh {
 
   async ohmyzsh() {
     await runSpawn(
-      `sh -c '$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)'`
+      `$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)'`
     );
   }
 }
