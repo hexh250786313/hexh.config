b89961101d620849ed792f867c003348a4495e27
hexh <250786313@qq.com>
2022-06-29 20:05:02 +0800

    wip

--- src/env/manjaro/modules/input-method/generate-fcitx-dir.ts
+++ src/env/manjaro/modules/input-method/generate-fcitx-dir.ts
@@ -1,16 +1,23 @@
 import runCommand from "@/utils/run-command";
 
 export default async function generateFcitxDir(timeout?: number) {
-  try {
-    await runCommand("pkill -e fcitx");
-  } catch (e) {
-    /* handle error */
-  }
-
-  // this step is to generate fcitx diretory and config file: ~/.config/fcitx
-  setTimeout(async () => {
-    await runCommand("pkill -e fcitx");
-  }, timeout || 10000);
-
-  await runCommand("fcitx &");
+  return new Promise((resolve) => {
+    (function attempt() {
+      runCommand("pkill -e fcitx")
+        .finally(async () => {
+          await runCommand("fcitx &");
+        })
+        .catch(() => {
+          //
+        });
+      setTimeout(async () => {
+        try {
+          await runCommand("pkill -e fcitx");
+        } catch (e: any) {
+          console.log(e + "\n");
+        }
+        resolve(null);
+      }, timeout || 60000);
+    })();
+  });
 }
