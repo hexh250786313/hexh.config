20e8bc0d8176d2a5a5a75034956578add4a549b1
hexh <250786313@qq.com>
2022-08-15 06:52:34 +0800

    wip

--- src/env/manjaro/modules/system/lock-wallpaper-config.ts
+++ src/env/manjaro/modules/system/lock-wallpaper-config.ts
@@ -1,3 +1,4 @@
 import { homedir } from "os";
 
-export const wallpaper = `${homedir()}/桌面/share/图片/993443.png`;
+// export const wallpaper = `${homedir()}/桌面/share/图片/993443.png`;
+export const wallpaper = `${homedir()}/桌面/share/图片/wallpapers/lock.jpg`;
