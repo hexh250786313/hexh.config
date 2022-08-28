bd9e5485da34d4b3d3ac29e1f7753e63ab2db4bd
hexh <250786313@qq.com>
2022-08-28 19:44:48 +0800

    wip

--- src/env/vm-ubuntu/modules/zsh.ts
+++ src/env/vm-ubuntu/modules/zsh.ts
@@ -1,4 +1,5 @@
 import runSpawn from "@/utils/run-spawn";
+import ln from "../../manjaro/ln";
 
 export default class Zsh {
   async run(args: string[]) {
@@ -25,10 +26,12 @@ export default class Zsh {
   }
 
   async ln() {
-    //
+    await ln(`/.zshrc`);
+    await ln(`/.warprc`);
   }
 
   async ohmyzsh() {
+    await runSpawn(`sudo apt install zsh`);
     await runSpawn(
       'sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"'
     );
