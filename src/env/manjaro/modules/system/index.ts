e85b4b71fd2ed419d03b4e27472fc0f1946b5419
hexh <250786313@qq.com>
2022-10-08 08:47:46 +0800

    wip

--- src/env/manjaro/modules/system/index.ts
+++ src/env/manjaro/modules/system/index.ts
@@ -203,7 +203,7 @@ export default class System {
   }
 
   async customCursor() {
-    const name = `material_dark_cursors`;
+    const name = `Bibata-Modern-Ice`;
     if (!existsSync(`/usr/share/icons/${name}`)) {
       const file = `${homedir()}/下载/${name}`;
       await runCommand(`tar -xzf ${file}.tar.gz -C ${homedir()}/下载`);
