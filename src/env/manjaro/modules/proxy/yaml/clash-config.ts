4a571408da197af1ec9c459a9a64b0bcf8bb3100
hexh <250786313@qq.com>
2024-08-26 15:21:11 +0800

    WIP

--- src/env/manjaro/modules/proxy/yaml/clash-config.ts
+++ src/env/manjaro/modules/proxy/yaml/clash-config.ts
@@ -1,10 +1,10 @@
 /* eslint-disable no-useless-escape */
-const _clashConfig = `mixed-port: 4780
+const _clashConfig = `mixed-port: 4781
 redir-port: 4782
 allow-lan: true
 mode: global
 log-level: info
-external-controller: \'0.0.0.0:9090\'
+external-controller: \'127.0.0.1:9091\'
 secret: \'\'
 dns:`;
 
